db324af98514582dc327414c0080707ed7f941fc
vckragstn <agustianvickri@gmail.com>
2024-03-20 00:30:06 +0700

    menambahkan section banner

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-const hambuger = document.querySelector('#hamburger');
-hambuger.addEventListener("click", (e) => {
-      const menu = document.querySelector('#navbar .menu');
-      menu.classList.toggle('active');
-})
